751301217d3c19004265d5cf5371e68e39448505
SrAxel <Thesense@outlook.es>
2024-06-03 15:35:50 -0300

    Cambios en la cadena de conexion del MySql

--- Program.cs
+++ Program.cs
@@ -13,8 +13,11 @@ builder.Services.AddControllersWithViews();
 //builder.Services.AddDbContext<InscripcionesContext>(options => options.UseSqlServer(configuration.GetConnectionString("sqlserver")));
 string cadenaConexion = configuration.GetConnectionString("mysqlremoto");
 builder.Services.AddDbContext<InscripcionesContext>(options => options.UseMySql(cadenaConexion,
-                                                    ServerVersion.AutoDetect(cadenaConexion)));
-
+            ServerVersion.AutoDetect(cadenaConexion),
+                                options => options.EnableRetryOnFailure(
+                                        maxRetryCount: 5,
+                                        maxRetryDelay: System.TimeSpan.FromSeconds(30),
+                                       errorNumbersToAdd: null)));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
